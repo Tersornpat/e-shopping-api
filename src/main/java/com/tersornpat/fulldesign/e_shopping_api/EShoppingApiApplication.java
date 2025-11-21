2f702ffb6e31076b1e5bdfb9a903797f875fa7c7
tersornpat <sornpat@gmail.com>
2025-11-21 14:30:52 +0700

    add: project initializer

---
+++ src/main/java/com/tersornpat/fulldesign/e_shopping_api/EShoppingApiApplication.java
@@ -0,0 +1,13 @@
+package com.tersornpat.fulldesign.e_shopping_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShoppingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShoppingApiApplication.class, args);
+	}
+
+}
